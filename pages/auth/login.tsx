65565773074fe7010f4c0b7e27bd9b55db745236
Mac Taylor <cslag@hotmail.com.tw>
2021-07-03 01:00:20 +0900

    Login methods modify

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 // layout for page
 
 import Auth from "../../layouts/Auth";
 
-// @ts-nocheck
-// const enable_other_way_loign = false;
+enum LoginMethodType {
+  Google,
+  UsernamePassowrd,
+}
 
 const Login = () => {
+  const [loginMethod, setLoginMethod] = useState<LoginMethodType>(
+    LoginMethodType.Google
+  );
+
+  const onLogin = () => {};
+
   return (
     <>
       <div className="container mx-auto px-4 h-full">
@@ -25,9 +33,16 @@ const Login = () => {
                   <button
                     className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
                     type="button"
+                    onClick={() =>
+                      setLoginMethod(LoginMethodType.UsernamePassowrd)
+                    }
                   >
-                    <img alt="..." className="w-5 mr-1" src="/img/github.svg" />
-                    Github
+                    <img
+                      alt="..."
+                      className="w-5 mr-1"
+                      src="/img/certificate.svg"
+                    />
+                    Credential
                   </button>
                   <button
                     className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
@@ -37,63 +52,69 @@ const Login = () => {
                     Google
                   </button>
                 </div>
-                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                {!!loginMethod &&
+                  loginMethod === LoginMethodType.UsernamePassowrd && (
+                    <hr className="mt-6 border-b-1 border-blueGray-300" />
+                  )}
               </div>
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div className="text-blueGray-400 text-center mb-3 font-bold">
-                  <small>Or sign in with credentials</small>
-                </div>
-                <form>
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Email"
-                    />
-                  </div>
+              {!!loginMethod &&
+                loginMethod === LoginMethodType.UsernamePassowrd && (
+                  <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                    <div className="text-blueGray-400 text-center mb-3 font-bold">
+                      <small>Or sign in with credentials</small>
+                    </div>
+                    <form>
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlFor="grid-password"
+                        >
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          placeholder="Email"
+                        />
+                      </div>
 
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div>
-                    <label className="inline-flex items-center cursor-pointer">
-                      <input
-                        id="customCheckLogin"
-                        type="checkbox"
-                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                      />
-                      <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                        Remember me
-                      </span>
-                    </label>
-                  </div>
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlFor="grid-password"
+                        >
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          placeholder="Password"
+                        />
+                      </div>
+                      <div>
+                        <label className="inline-flex items-center cursor-pointer">
+                          <input
+                            id="customCheckLogin"
+                            type="checkbox"
+                            className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                          />
+                          <span className="ml-2 text-sm font-semibold text-blueGray-600">
+                            Remember me
+                          </span>
+                        </label>
+                      </div>
 
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      Sign In
-                    </button>
+                      <div className="text-center mt-6">
+                        <button
+                          className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                          type="button"
+                        >
+                          Sign In
+                        </button>
+                      </div>
+                    </form>
                   </div>
-                </form>
-              </div>
+                )}
             </div>
             <div className="flex flex-wrap mt-6 relative">
               <div className="w-1/2">
@@ -118,7 +139,7 @@ const Login = () => {
       </div>
     </>
   );
-}
+};
 
 export default Login;
 
