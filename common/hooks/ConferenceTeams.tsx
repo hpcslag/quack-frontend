ec18f276248c4aa712eec8d2219f40c8836666a9
Mac Taylor <cslag@hotmail.com.tw>
2021-08-14 17:38:45 +0900

    list_team to hook

---
+++ common/hooks/ConferenceTeams.tsx
@@ -0,0 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
+
+const CONFERENCE_TEAM_QUERY = gql`
+  query ConferenceTeams($conference_id: ID!) {
+    teams(conferenceId: $conference_id) {
+      team_id
+      team_name
+      __typename
+    }
+  }
+`;
+
+interface CONFERENCE_TEAM_QUERY_RESULT {
+  teams: {
+    __typename: "Team";
+    team_id: number;
+    team_name: string;
+  };
+}
+
+export const useConferenceTeams = (conference_id: number | string) => {
+  const { data } = useQuery<CONFERENCE_TEAM_QUERY_RESULT, any>(
+    CONFERENCE_TEAM_QUERY,
+    {
+      variables: {
+        conference_id: conference_id,
+      },
+    }
+  );
+  if (data?.teams) {
+    return data.teams;
+  }
+  return [];
+};
