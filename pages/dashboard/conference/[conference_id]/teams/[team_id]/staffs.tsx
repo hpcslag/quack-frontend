06c0c9724ecde68f027560f070a6e7332f42cf3d
Mac Taylor <cslag@hotmail.com.tw>
2021-08-18 19:13:40 +0900

    send notifications

--- pages/dashboard/conference/[conference_id]/teams/[team_id]/staffs.tsx
+++ pages/dashboard/conference/[conference_id]/teams/[team_id]/staffs.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 // components
@@ -9,12 +10,28 @@ import ListStaff from "../../../../../../components/Staffs/ListStaff";
 import PureDark from "../../../../../../layouts/PureDark";
 
 const StaffTable = () => {
+  const router = useRouter();
   const staffs = [{}];
+
+  const sendNotificationToTeamMember = () => {
+    router.push({
+      pathname: "/dashboard/notification/notify-plan",
+      query: { to: [1, 2, 3] },
+    });
+  };
+
   return (
     <>
       <div className="flex flex-wrap">
         <div className="w-full mb-12 px-4">
           <ListStaff />
+          <button
+            className="bg-blueGray-500 text-white active:bg-blueGray-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+            onClick={sendNotificationToTeamMember}
+          >
+            發送通知
+          </button>
         </div>
       </div>
     </>
